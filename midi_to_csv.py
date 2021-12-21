9f9e296c5c486e30536586666afaef1afac8923e
vinceent505 <vinceent505@gmail.com>
2021-12-21 14:40:15 +0800

    12/21

--- midi_to_csv.py
+++ midi_to_csv.py
@@ -5,6 +5,7 @@ import audiolazy
 
 def main():
 	filename = str(sys.argv[1])
+	print(filename)
 	mid = mido.MidiFile(filename)
 	start = []
 	end = []
@@ -12,40 +13,32 @@ def main():
 	first = True
 	noteon_midi = []
 	for i in mid:
-		if i.type == "note_on":
+		if i.type == "note_on" or i.type == "note_off":
 			noteon_midi.append(i)
 
 	for i in range(0, len(noteon_midi), 2):
-		note.append(noteon_midi.note)
+		note.append(noteon_midi[i].note)
 		if first:
 			start.append(noteon_midi[i].time)
-			end.append(noteon_midi[i+1].time + start[i/2])
+			end.append(noteon_midi[i+1].time + start[i//2])
 			first = False
-			pass
 		else:
-			start.append(end[i/2-1])
-			end.append(noteon_midi[i+1].time + start[i/2])
-			pass
-		pass
-	t = []
-	print(len(start))
-	print(len(note))
-	for i in range(len(time)):
-		if i>0:
-			time[i] += time[i-1]
-	for i in time:
-		t.append([i])
-	tt = []
-	for i in range(len(t)):
-		tt.append([t[i][0] - t[0][0], audiolazy.midi2str(note[i])])
-	
-	name = ["start", "note"]
-	with open("midi_output_csvs/" + filename[:-4] + ".csv", "w") as f:
-		writer = csv.writer(f)
-		writer.writerow(name)
-		writer.writerows(tt)
+			start.append(end[i//2-1])
+			end.append(noteon_midi[i+1].time + start[i//2])
 
-	pass
+	out = []
+	for i in range(len(start)):
+		out.append([i, start[i], audiolazy.midi2str(note[i])])
+	filename_output = "midi_output_csvs/" + filename[:-4] + ".csv"
+	print(filename_output)
+	name = ["num","start", "note"]
+	with open(filename, "w") as f:
+		# writer = csv.writer(f)
+		# writer.writerow(name)
+		# writer.writerows(out)
+		pass
+
+	
 
 if __name__ == "__main__":
 	main()
